bfc50168695953f73aea2b3850c579c22126ebe9
NathanaelLins <nathanael.lins@student.htldornbirn.at>
2023-12-07 16:38:16 +0100

    Informatik

---
+++ src/at/NL/Struktur/oop/Katze.java
@@ -0,0 +1,60 @@
+package at.NL.Struktur.oop;
+
+public class Katze {
+    private String character;
+    private String oberflaechenbeschaffenheit;
+    private int alter;
+    private String farbe;
+    private String name;
+
+    public Katze() {
+    }
+
+    public Katze(String character, String oberflaechenbeschaffenheit, int alter, String farbe, String name) {
+        this.character = character;
+        this.oberflaechenbeschaffenheit = oberflaechenbeschaffenheit;
+        this.alter = alter;
+        this.farbe = farbe;
+        this.name = name;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public String getOberflaechenbeschaffenheit() {
+        return oberflaechenbeschaffenheit;
+    }
+
+    public void setOberflaechenbeschaffenheit(String oberflaechenbeschaffenheit) {
+        this.oberflaechenbeschaffenheit = oberflaechenbeschaffenheit;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
+    public String getFarbe() {
+        return farbe;
+    }
+
+    public void setFarbe(String farbe) {
+        this.farbe = farbe;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/at/NL/Struktur/oop/TestIt.java
@@ -0,0 +1,42 @@
+package at.NL.Struktur.oop;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TestIt {
+    public static void main(String[] args){
+        Random r = new Random();
+        ArrayList<Katze> mKatzenListe = new ArrayList();
+        Katze katze = new Katze();
+        String[] charListe ={"bösartig","gutartig","hinterhältig","listig","schmusig","lieblich"};
+        String[] oberListe ={"weich","hart","nass","hässlich","Elias Frisur","wie Elias hässlich Katze"};
+        String[] namenListe ={"Elias","Elija","Marvin","Dine Mom","Non-Binare","Kratzer in Elijas Handy"};
+        String[] farbeListe ={"weiß","schwarz","beides","alles","rainbow","hässlich"};
+
+
+        for (int i=1; i<=100; i++){
+            int ci = r.nextInt(charListe.length - 1 -0)+0;
+            int oi = r.nextInt(oberListe.length - 1 -0)+0;
+            int ni = r.nextInt(namenListe.length - 1 -0)+0;
+            int fi = r.nextInt(farbeListe.length - 1 -0)+0;
+            int ai = r.nextInt(25 + 1 -0)+0;
+
+            // mKatzenListe.add(new Katze(charListe[0],"nackt",5,"rosa","Nackibutz"));
+
+        }
+        //mKatzenListe.add(katze);
+        // katze.setAlter(1);
+       // katze.setCharacter("bösartig");
+       // katze.setFarbe("karriert");
+       // katze.setOberflaechenbeschaffenheit("flauschig");
+       // katze.setName("Mitzi");
+
+        Katze katze2 = new Katze("nette","nackt",5,"rosa","Nackibutz");
+        mKatzenListe.add(katze2);
+        // System.out.println("Meine Katze "+katze.getName() +" ist "+ katze.getAlter() + " alt!");
+
+        for(Katze k: mKatzenListe){
+            System.out.println("Meine Katze "+k.getName() +" ist "+ k.getAlter() + " alt," + k.getCharacter() + ", "+k.getName() + ", " + k.getOberflaechenbeschaffenheit());
+        }
+    }
+}
